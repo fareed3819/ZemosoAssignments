4bd3f6c24ed98727cee08d1d7ae8417f3980a5a5
fareedsheik <59389608+fareedsheik@users.noreply.github.com>
2020-02-18 11:11:04 +0530

    Assignment6 part2 done

--- javaAssignment/JavaAssignment2020/assignment6/Assign6_2.java
+++ javaAssignment/JavaAssignment2020/assignment6/Assign6_2.java
@@ -2,17 +2,17 @@
 2. Create a class with two (overloaded) constructors. Using this, call the second constructor inside the first one.
  */
 package assignment6;
-class A{
-    public A(){
+class Demo1{
+    public Demo1(){
         this("Overloaded constructer called from default one");
     }
 
-    public A(String s){
+    public Demo1(String s){
         System.out.println(s);
     }
 
     public static void main(String args[]){
-        new A();
+        new Demo1();
     }
 
 }
