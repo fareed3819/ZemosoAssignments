949463897ac8a579816989ec27f159ef44db6426
fareedsheik <59389608+fareedsheik@users.noreply.github.com>
2020-02-18 11:25:17 +0530

    Assignment 8 done

--- javaAssignment/JavaAssignment2020/assignment8/Exceptions.java
+++ javaAssignment/JavaAssignment2020/assignment8/Exceptions.java
@@ -1,36 +1,39 @@
+
+/*
+
+Create three new types of exceptions. Write a class with a method that throws all three. In main( ), call the method but only use a single catch clause that will catch all three types of exceptions. Add a finally clause and verify that your finally clause is executed, even if a NullPointerException is thrown.
+*/
 package assignment8;
 
 import java.util.*;
 class Exception1 extends Exception{
-    public Exception1(String s)
+    public Exception1(String info)
     {
-        super(s);
+        super(info);
     }
 }
 class Exception2 extends Exception{
-    public Exception2(String s)
+    public Exception2(String info)
     {
-        super(s);
+        super(info);
     }
 }
 class Exception3 extends Exception{
-    public Exception3(String s)
+    public Exception3(String info)
     {
-        super(s);
+        super(info);
     }
 }
 
 class TestException{
-    public static void throwsExp(String s) throws Exception1,Exception2,Exception3,Exception{
-        if(s.equalsIgnoreCase("exception1")) throw new Exception1("custom Exception 1 ");
-        else if(s.equalsIgnoreCase("exception2")) throw new Exception2("custom Exception 2 ");
-        else if(s.equalsIgnoreCase("exception3")) throw new Exception3("custom Exception 3 ");
-        String c=null;
-        c.toString();
+    public static void throwsExp(String exceptionName) throws Exception1,Exception2,Exception3,Exception{
+        if(exceptionName.equalsIgnoreCase("exception1")) throw new Exception1("custom Exception 1 ");
+        else if(exceptionName.equalsIgnoreCase("exception2")) throw new Exception2("custom Exception 2 ");
+        else if(exceptionName.equalsIgnoreCase("exception3")) throw new Exception3("custom Exception 3 ");
 
     }
     public static void main(String args[]){
-        String name=args[0];
+        String exceptionName=args[0];
         try{
             throwsExp(name);
         }
