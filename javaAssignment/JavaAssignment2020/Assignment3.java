f680f1cb2b0f72c991b5a343ff41443318d02a81
fareedsheik <59389608+fareedsheik@users.noreply.github.com>
2020-02-18 10:47:26 +0530

    Assignment 3 done

--- javaAssignment/JavaAssignment2020/Assignment3.java
+++ javaAssignment/JavaAssignment2020/Assignment3.java
@@ -3,20 +3,21 @@ Write a java function that will ping any host ( given as input ) and computes th
 
 Use the system ping utility, do not use any deprecated methods.
  */
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class Assignment3 {
-    static void ping(String host,int no_of_times)
-    {
-        String command="ping -c "+no_of_times+" "+host;
-        String command_output="";
-        int counter=0;
-        int index=0;
-        String time="";
-        ArrayList<Float> arrayList=new ArrayList<Float>();
+
+    public static void ping(String host, int no_of_times) {
+        String command = "ping -c " + no_of_times + " " + host;
+        String command_output = "";
+        int counter = 0;
+        int index = 0;
+        String time = "";
+        ArrayList<Float> timeTaken = new ArrayList<Float>();
         try {
             Process process = Runtime.getRuntime().exec(command);
             BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
@@ -25,27 +26,28 @@ public class Assignment3 {
             while (counter < no_of_times) {
                 index = command_output.indexOf("time");
                 time = command_output.substring(index + 5, index + 9);
-                arrayList.add(Float.parseFloat(time));
+                timeTaken.add(Float.parseFloat(time));
                 command_output = stdInput.readLine();
                 counter++;
 
             }
-            Collections.sort(arrayList);
-            if (arrayList.size() % 2 != 0)
-                System.out.println("Median of time taken to ping " + arrayList.get(arrayList.size() / 2));
-            else
-                System.out.println("Median of time taken to ping " + (arrayList.get(arrayList.size() / 2) + arrayList.get((arrayList.size() / 2) - 1)) / 2);
-        }
-        catch (Exception e){e.printStackTrace();}
 
+            Collections.sort(timeTaken);
+            if (timeTaken.size() % 2 != 0)
+                System.out.println("Median of time taken to ping " + timeTaken.get(timeTaken.size() / 2));
+            else
+                System.out.println("Median of time taken to ping " + (timeTaken.get(timeTaken.size() / 2) + timeTaken.get((timeTaken.size() / 2) - 1)) / 2);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-    public static void main(String[] args) throws Exception{
-        BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Enter host to ping");
-        String host=bufferedReader.readLine();
+        String host = bufferedReader.readLine();
         System.out.println("Enter number of times to ping");
-        int no_of_times=Integer.parseInt(bufferedReader.readLine().trim());
-        ping(host,no_of_times);
+        int no_of_times = Integer.parseInt(bufferedReader.readLine().trim());
+        ping(host, no_of_times);
 
     }
 }
