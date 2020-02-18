c6aed52154d19190b60fe88fb35266b1921cbf1b
fareedsheik <59389608+fareedsheik@users.noreply.github.com>
2020-02-18 11:35:14 +0530

    Assignment 9 done

--- javaAssignment/JavaAssignment2020/Assignment9/RegEx.java
+++ javaAssignment/JavaAssignment2020/Assignment9/RegEx.java
@@ -2,17 +2,27 @@
 Using the documentation for java.util.regex.Pattern as a resource, write and test a regular expression that checks a sentence to see that it begins with a capital letter and ends with a period.
  */
 package Assignment9;
+
+import javax.imageio.ImageTranscoder;
 import java.util.*;
 import java.util.regex.*;
-
-class RegEx{
+class RegExCheck{
+    public boolean matchRegex(String regex,String input)
+    {
+        Pattern p = Pattern.compile(regex);
+        Matcher m=p.matcher(input);
+        if(m.find()) return true;
+        return false;
+    }
     public static void main(String args[]){
-        String inputRegex="^[A-Z].*[.]";
+        String inputRegex="^[A-Z].*$[.]";
        // String line=args[0];
-        String line="Ads342.";   //testing input
-        Pattern pattern = Pattern.compile(inputRegex);
-        Matcher matcher=pattern.matcher(line);
-        if(matcher.find()) System.out.println("found");
-        else System.out.println("not found");
+        String input="Ads342./";
+
+        if(new RegExCheck().matchRegex(inputRegex,input))
+            System.out.println("regex Matched");
+        else
+            System.out.println("unmatched sentence");
+
     }
 }
