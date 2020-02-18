9c142edcd5de170eefcc2aac27b3f1eb01293cb9
fareedsheik <59389608+fareedsheik@users.noreply.github.com>
2020-02-18 10:24:47 +0530

    Courceera assignment 10 on generics done

--- javaAssignment/JavaAssignment2020/assignment10/Generics.java
+++ javaAssignment/JavaAssignment2020/assignment10/Generics.java
@@ -12,8 +12,6 @@ class Node<T>
         next=null;
     }
 }
-
-
 class SLIst<T>
 {
     Node<T> head;
@@ -28,107 +26,104 @@ class SLIst<T>
     }
 }
 
-class SLIstIterator<T>
-{
+class SLIstIterator<T> {
     Node<T> head;
     Node<T> current;
     int count;
-    SLIstIterator(Node<T> head){
-        this.head=head;
+
+    SLIstIterator(Node<T> head) {
+        this.head = head;
     }
 
-    public void insert(T data){
+    public void insert(T data) {
         count++;
-        if (head==null){
-            head=new Node<T>(data);
-            current=head;
+        if (head == null) {
+            head = new Node<T>(data);
+            current = head;
+        } else {
+            current.next = new Node<T>(data);
+            current = current.next;
         }
-        else{
-            current.next=new Node<T>(data);
-            current=current.next;
-        }
-        System.out.println("item inserted:"+data);
+        System.out.println("item inserted:" + data);
     }
 
 
-    public void delete(T data){
-        if (head==null){
-            System.out.println("List is empty: Cannot forward delete opeartion of item"+data);
+    public void delete(T data) {
+        if (head == null) {
+            System.out.println("List is empty: Cannot forward delete opeartion of item" + data);
             return;
         }
-        if(String.valueOf(head.data).equals(String.valueOf(data))){
-            head=head.next;
+        if (String.valueOf(head.data).equals(String.valueOf(data))) {
+            head = head.next;
             count--;
-            System.out.println("item deleted:"+data);
+            System.out.println("item deleted:" + data);
             return;
         }
-        Node<T> pre=head;
-        Node<T> temp=null;
-        while(pre.next!=null){
-            temp=pre.next;
-            if(  String.valueOf(temp.data).equals(String.valueOf(data)) ) {
-                pre.next=temp.next;
+        Node<T> previous = head;
+        Node<T> temp = null;
+        while (previous.next != null) {
+            temp = previous.next;
+            if (String.valueOf(temp.data).equals(String.valueOf(data))) {
+                previous.next = temp.next;
                 count--;
-                System.out.println("item deleted:"+data);
+                System.out.println("item deleted: " + data);
                 return;
-            }
-            else
-                pre=pre.next;
+            } else
+                previous = previous.next;
         }
-        System.out.println("item not found:"+data);
+        System.out.println("item not found:" + data);
     }
 
-    public void display(){
-        if(head==null){
+    public void display() {
+        if (head == null) {
             System.out.println("Empty list:[]");
             return;
         }
-        Node<T> temp=head;
+        Node<T> temp = head;
         System.out.print("[");
-        while(temp!=null){
-            System.out.print(" "+temp.data+",");
-            temp=temp.next;
+        while (temp != null) {
+            System.out.print(" " + temp.data + ",");
+            temp = temp.next;
         }
         System.out.println("]");
     }
-    public void size(){
-        System.out.println("size is :"+count);
+
+    public void size() {
+
+        System.out.println("size is :" + count);
     }
 }
+
 
 class Generics{
     public static void main(String args[])
     {
         SLIst<Integer> st = new SLIst<Integer>();
-        SLIstIterator it  = st.getIterator();
-        it.insert(10);
-        it.insert(20);
-        it.insert(30);
-        it.insert(40);
-        it.insert(50);
-        it.insert(60);
-        it.size();
-        it.display();
-        it.delete(60);
-        it.delete(30);
-        it.delete(90);
-        it.delete(10);
-        it.size();
-        it.display();
-        SLIst<String> st1 = new SLIst<String>();
-        SLIstIterator it1 = st1.getIterator();
-        it1.insert("rahul");
-        it1.insert("sreetej");
-        it1.insert("kotha");
-        it1.insert("jayesh");
-        it1.insert("griet");
-        it1.insert("cse");
-        it1.size();
-        it1.display();
-        it1.delete("sreetej");
-        it1.delete("griet");
-        it1.delete("hyderabad");
-        it1.size();
-        it1.display();
+        SLIstIterator iterator  = st.getIterator();
+
+        Scanner scanner=new Scanner(System.in);
+        int choice=1;
+        while(true) {
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter data\n");
+                    iterator.insert(scanner.nextInt());
+                    break;
+
+                case 2:
+                    iterator.size();
+                    break;
+                case 3:
+                    iterator.display();
+                    break;
+
+                case 4:System.out.println("Enter data to delete ");
+                    iterator.delete(scanner.nextInt());break;
+            }
+            System.out.println(" \n  Select \n 1 .Insert \n 2. Size \n 3.Display 4.Delete \n");
+            choice=scanner.nextInt();
+        }
+
+
     }
 }
