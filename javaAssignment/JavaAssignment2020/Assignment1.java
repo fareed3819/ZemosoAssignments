3d911d6ef9a65db547e4ce3db9dbe0b055048ebb
fareedsheik <sheikfareed3819@gmail.com>
2020-03-09 19:26:37 +0530

    updated

--- javaAssignment/JavaAssignment2020/Assignment1.java
+++ javaAssignment/JavaAssignment2020/Assignment1.java
@@ -1,42 +1,57 @@
+package Assignment1;
+
+
 /*
 Create a java program to search through the home directory and look for files that match a regular expression. The program should be able to take inputs repeatedly and should print out the full absolute path of the matching files found.
-
 Provide appropriate documentation and comments on your code.
  */
 
 import java.io.*;
 import java.util.regex.*;
-class Assignment1
-{
+import java.util.logging.Logger;
+
+class MatchingFiles {
     static Pattern pattern;
-    
-    public static void search(String path,String regex)
-    {
+    static Logger logger = Logger.getLogger(MatchingFiles.class.getName());
+
+    /*
+    This method takes path and regular expression as parameters .
+    it checks all the files exists in path recurrsively and stores the result of matched files absolutepath into string.
+    It concatenates all the matched files absolute paths and returns the result as a string.
+     */
+
+    public static String searchForMatchedFiles(String path, String regex) {
+        String res = "";
         File folder = new File(path);
         pattern = Pattern.compile(regex);
-       // String result="not found";
-        for(File file:folder.listFiles())
-        {
-         
-            if(file.isFile() && pattern.matcher(file.getName()).matches())
-                System.out.println(file.getAbsolutePath());////get  file.getAbsolutePath() if it is file and matches regex
-
-                
-            else if(file.isDirectory())//if it is directrory perfrom search again
-                search(file.getAbsolutePath(),regex);
+        for (File file : folder.listFiles()) {
+            if (file.isHidden())
+                continue;
+            if (file.isFile() && pattern.matcher(file.getName()).matches()) {
+                logger.info("regex matched file found and  its absolute path is" + file.getAbsolutePath());
+                res = res + file.getAbsolutePath();
+            }
+            else if (file.isDirectory()) {
+                searchForMatchedFiles(file.getAbsolutePath(), regex);
+            }
         }
+        if (res != "")
+            return res;
 
-
+        return "Not found";
     }
-    public static void main(String []args)throws Exception
-    {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        while(true)
-        {
-            System.out.println("Enter the regex to search (CTRL+C to quit): ");
-            String regex = br.readLine();
-            //passing home directory to search all directories recurrsively
-            search(System.getProperty("user.home"),regex);
+
+    public static void main(String[] args) {
+        try {
+            while (true) {
+                System.out.println("Enter the regex to search (CTRL+C to quit): ");
+                String regex = new BufferedReader(new InputStreamReader(System.in)).readLine();
+                String result = searchForMatchedFiles(System.getProperty("user.home"), regex);
+                logger.info("result is" + result);
+            }
+        } catch (Exception e) {
+            logger.info("Exception occured this may due to reading files having no read permissions or input error");
+            e.printStackTrace();
         }
     }
 }
--- javaAssignment/JavaAssignment2020/Assignment2.java
+++ javaAssignment/JavaAssignment2020/Assignment2.java
@@ -1,34 +1,41 @@
-/*
-Write a java function that checks if the input string contains all the letters of the alphabet a-z (case-insensitive). Write time and space complexity of your solution as comments in the source file.
- */
-//Time complexity - O(N), Space Complexity - O(1)
-// where N = length of input string
+package Assignment2;
 
-import java.util.*;
-import java.io.*;
-class Assignment2
-{   
-    public static void check(char inputArray[])
-    {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.logging.Logger;
+
+public class SmallAlphabet {
+    static Logger logger = Logger.getLogger(SmallAlphabet.class.getName());
+
+    /*
+    This method adds unique characters from input to set and checks
+    for existance of all small letters(a-z) using for loop
+     */
+    public static boolean checkAllAlphabetOccurrence(char inputArray[]) {
         Set<Character> set = new HashSet<>();
-        //add all unique input characters to set 
-        for(char character:inputArray)
+        for (char character : inputArray)
             set.add(character);
-        //check a-z characters present in set 
-        for(char character='a';character<='z';character++)
-            if(!set.contains(character))
-            {
-                System.out.println("search breaked at "+character+" because it is not found");System.exit(-1);
+        for (char character = 'a'; character <= 'z'; character++)
+            if (!set.contains(character)) {
+                logger.info("search breaked at " + character + " because it is not found");
+                return false;
             }
-        System.out.println("All small alphabet are present in input");
+        logger.info("all small alphabets are present");
+        return true;
     }
-    public static void main(String []args)throws IOException
-    {
 
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Enter the input string: ");
-        char inputString[] = bufferedReader.readLine().toLowerCase().toCharArray();
-        check(inputString);
+    public static void main(String[] args) {
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Enter the input string: ");
+            char inputString[] = bufferedReader.readLine().toLowerCase().toCharArray();
+            boolean result = checkAllAlphabetOccurrence(inputString);
+            logger.info("result is  " + result);
 
+        } catch (Exception e) {
+            logger.info("Exception ocurred at input ");
+        }
     }
-} 
+}
